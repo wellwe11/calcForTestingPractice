19011366af523bc1b48d7a171f9bd6b8781e261e
Robin Ryan <robin1ryan@hotmail.com>
2025-06-07 14:05:24 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+import CalculatorContainer from "./CALCULATOR/CONTAINER/CALC_CONTAINER/container";
+
+function App() {
+  return (
+    <div>
+      <CalculatorContainer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/CALCULATOR/CONTAINER/CALC_CONTAINER/container.jsx
@@ -0,0 +1,11 @@
+import "./container.scss";
+
+const CalculatorContainer = () => {
+  return (
+    <div className="calculatorContainer">
+      <h1>container</h1>
+    </div>
+  );
+};
+
+export default CalculatorContainer;
